12d394e74a90d640af08d053eeb901160fd537c0
restu <restuaufacahyadin02@gmail.com>
2021-12-18 19:05:27 +0700

    tugas intent restu

---
+++ LatihanIntentSederhana_MRestuAufaC/app/src/main/java/com/example/latihanintentsederhana_mrestuaufac/IntentActivity.java
@@ -0,0 +1,13 @@
+package com.example.latihanintentsederhana_mrestuaufac;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
---
+++ LatihanPOJO_MRestuAufaCahyadin/app/src/main/java/com/example/latihanpojo_mrestuaufacahyadin/MoveActivity.java
@@ -0,0 +1,13 @@
+package com.example.latihanpojo_mrestuaufacahyadin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
